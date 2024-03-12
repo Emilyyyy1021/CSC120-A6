82e80ffdd26d9b5a3fea264d52f76a3596137a8b
Emilyyyy1021 <ewang888@smith.edu>
2024-03-11 22:24:48 -0400

    finished

--- House.java
+++ House.java
@@ -1,12 +1,81 @@
-/* This is a stub for the House class */
-public class House {
+/* the House Class */
+import java.util.ArrayList;
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+public class House extends Building{
+
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom, ArrayList<String> residents) {
+    super(name, address, nFloors);
+    this.hasDiningRoom = hasDiningRoom;
+    this.residents = residents;
+    
   }
 
-  public static void main(String[] args) {
-    new House();
+  /**
+   * Getter for whether has a dinning room 
+   * @return T/F has a dinning room
+   */
+  public boolean hasDiningRoom() {
+    return this.hasDiningRoom;
   }
 
+  /**
+   * Getter for number of residents 
+   * @return number of residents 
+   */
+  public int nResidents() {
+    try{
+      return this.residents.size();
+  }catch (Exception e){
+    System.out.println(e);
+    return 0;
+  }
+  }
+
+  /**
+   * resident movein
+   * @param name name of resident
+   */
+  public void moveIn(String name){
+    if (isResident(name) == false){
+      this.residents.add(name);
+    }
+  }
+
+  /**
+   * resident moveout
+   * @param name the name of resident
+   * @return name the name of resident who moved out
+   */
+  // return the name of the person who moved out
+  public String moveOut(String name){
+    if (isResident(name) == true){
+      this.residents.remove(name);
+      return name;
+    }else {
+      return name + "Not found";
+    }
+  }
+
+  /**
+   * the resident is in the house
+   * @param person the resident 
+   * @return T/F if the resident is the list
+   */
+  public boolean isResident(String person){
+    if (this.residents.contains(person)){
+      System.out.println(person + " is in " + name);
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+
+  // public static void main(String[] args) {
+  //   House Ziskind = new House("Ziskind", "1 Henshaw Ave", 3, true, new ArrayList<residents>());
+  // }
 }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,7 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+I would be able to do less work. Although this homework doesn't have much inheritance with the functions, but I can see how the classes influence with each other. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I was able to grasp how hastable works very quickly, which surprised me a lot. I still do not understand how inheritance works with the functions. Learn the concepts better, because at this stage coding seems fine. 
